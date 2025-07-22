39c9aed5057ec01038eac7d346d067ca1c4e50dc
itsmeAthul20 <athulharidas21@gmail.com>
2025-07-22 10:40:44 +0530

    initial commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,47 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              {" "}
+              Athul
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              {" "}
+              Haridas
+            </span>
+          </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+           I’m a dedicated and detail-oriented web developer and software tester,
+            passionate about building responsive web applications and ensuring their 
+            quality through effective testing. With a strong foundation in HTML, CSS,
+             JavaScript, and testing practices, I strive to deliver user-friendly,
+              high-performance digital experiences that are both visually engaging and bug-free.
+
+
+          </p>
+
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
